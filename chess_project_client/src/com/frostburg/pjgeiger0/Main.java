64d3d2c948f7e730ec43f64a80bddac59ab795df
Patrick Geiger <patrickjgeiger@gmail.com>
2018-04-23 10:19:52 -0400

    Multiple clients on one server
    Set up the ability to have many clients access one server by using
    threading.

--- chess_project_client/src/com/frostburg/pjgeiger0/Main.java
+++ chess_project_client/src/com/frostburg/pjgeiger0/Main.java
@@ -1,37 +1,43 @@
 package com.frostburg.pjgeiger0;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String host_name = "127.0.0.1";
         int port_number = 4000;
-        try {
-            Socket socket = new Socket(host_name, port_number);
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedReader standard_input = new BufferedReader(new InputStreamReader(System.in));
+        try{
+            Scanner system_input = new Scanner(System.in);
 
-            String user_input;
-            while((user_input = standard_input.readLine()) != null){
-                output.println(user_input);
-                System.out.println("echo: " + input.readLine());
+            InetAddress ip = InetAddress.getByName("localhost");
+
+            Socket client = new Socket(ip, port_number);
+            DataInputStream input = new DataInputStream(client.getInputStream());
+            DataOutputStream output = new DataOutputStream(client.getOutputStream());
+
+            while(true){
+                System.out.println(input.readUTF());
+                String sending = system_input.nextLine();
+                output.writeUTF(sending);
+
+                if(sending.equals("Exit")){
+                    System.out.println("closing the connection: " + client);
+                    client.close();
+                    System.out.println("connection closed");
+                    break;
+                }
             }
-
-            socket.close();
-            output.close();
+            system_input.close();
             input.close();
-            standard_input.close();
+            output.close();
         }
         catch(IOException e){
-            System.out.println(e);
-            System.exit(1);
+            e.printStackTrace();
         }
+
     }
 }
--- chess_project_server/src/com/frostburg/pjgeiger0/Main.java
+++ chess_project_server/src/com/frostburg/pjgeiger0/Main.java
@@ -1,42 +1,90 @@
 package com.frostburg.pjgeiger0;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
         ServerSocket server;
-        Socket client;
-        PrintWriter output;
-        BufferedReader input;
         int port_number = 4000;
-        try {
-            server = new ServerSocket(port_number);
-            System.out.println("Server listening on port: " + port_number);
-            client = server.accept();
-            output = new PrintWriter(client.getOutputStream(), true);
-            input = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-            System.out.println("Client connected on port " + port_number + ", servicing reuqests.");
-            String input_line;
-            while((input_line = input.readLine()) != null){
-                System.out.println(("Received message " + input_line + " from " + client.toString()));
-                output.println(input_line);
+        server = new ServerSocket(port_number);
+        while(true){
+            Socket client = null;
+            try{
+                client = server.accept();
+
+                System.out.println("A new client has connected: " + client);
+
+                DataInputStream input = new DataInputStream(client.getInputStream());
+                DataOutputStream output = new DataOutputStream(client.getOutputStream());
+
+                System.out.println("Assigning new thread to this client");
+
+                Thread t = new ClientHandler(client, input, output);
+
+                t.start();
             }
+            catch(Exception e){
+                client.close();
+                e.printStackTrace();
+            }
+        }
 
-            server.close();
-            client.close();
-            output.close();
-            input.close();
-        }
-        catch(IOException e){
-            System.out.println(e);
-        }
     }
+}
+
+class ClientHandler extends Thread
+{
+    final DataInputStream input_stream;
+    final DataOutputStream output_stream;
+    final Socket client_socket;
+
+    public ClientHandler(Socket client, DataInputStream input, DataOutputStream output){
+        input_stream = input;
+        output_stream = output;
+        client_socket = client;
+    }
+
+    @Override
+    public void run(){
+        String receiving;
+        String returning;
+
+        while(true){
+            try {
+                output_stream.writeUTF("Pong");
+                receiving = input_stream.readUTF();
+
+                if(receiving.equals("Exit")){
+                    System.out.println("Closing connection with: " + client_socket);
+                    client_socket.close();
+                    System.out.println("Connection closed");
+                    break;
+                }
+                if(receiving.equals("Ping")){
+                    returning = "Pong";
+                    output_stream.writeUTF(returning);
+                    break;
+                }
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        try{
+            input_stream.close();
+            output_stream.close();
+            client_socket.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
 }
