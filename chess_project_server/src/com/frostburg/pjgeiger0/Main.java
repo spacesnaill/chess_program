a16d97736762711bdbf5548a74d082c7659ac168
Patrick Geiger <patrickjgeiger@gmail.com>
2018-05-07 17:26:29 -0400

    game setup now possible

--- chess_project_server/src/com/frostburg/pjgeiger0/Main.java
+++ chess_project_server/src/com/frostburg/pjgeiger0/Main.java
@@ -66,7 +66,7 @@ public class Main {
         }
 
         //might reverse this so the coordinates can be multiplied by each other to get the location in the array
-        String[][] board = new String[][]{
+        private String[][] board = new String[][]{
                 {"A8", "BR1"}, {"B8", "BK1"}, {"C8", "BB1"}, {"D8", "BKing"}, {"E8", "BQueen"}, {"F8", "BB2"}, {"G8", "BK2"}, {"H8", "BR2"},
                 {"A7", "BP1"}, {"B7", "BP2"}, {"C7", "BP3"}, {"D7", "BP4"}, {"E7", "BP5"}, {"F7", "BP6"}, {"G7", "BP7"}, {"H7", "BP8"},
                 {"A6", ".."}, {"B6", "__"}, {"C6", ".."}, {"D6", "__"}, {"E6", ".."}, {"F6", "__"}, {"G6", ".."}, {"H6", "__"},
@@ -78,13 +78,20 @@ public class Main {
         };
 
 
+
         @Override
         public void run() {
 
         }
 
-        public void movePiece(String piece, String currentLocation, String destination) {
 
+        public void movePiece(ClientHandler player, String) throws IOException {
+            if(player == player_one){
+                player_one.messageToClient("Piece moved");
+            }
+            else{
+                player_two.messageToClient("");
+            }
         }
     }
 
@@ -117,11 +124,17 @@ public class Main {
         public void createMatch(String oppenent) throws IOException{
             try {
                 match = new MatchHandler(this, clients.get(oppenent));
-                
+                in_game = true;
+                clients.get(oppenent).setMatch(match);
+                clients.get(oppenent).in_game = true;
             }
             catch(NullPointerException e){
                 output_stream.writeUTF("No user by that name found.");
             }
+        }
+
+        public void setMatch(MatchHandler newMatch){
+            match = newMatch;
         }
 
         public void listUsers() throws IOException {
@@ -228,7 +241,9 @@ public class Main {
                             if(in_game){
                                 messageToClient("Sorry, you're already in a game");
                             }
-                            createMatch();
+                            else {
+                                createMatch(messageArray[1]);
+                            }
                         }
                         else if(messageArray[0].equals("--leave")){
                             //leaves a game
